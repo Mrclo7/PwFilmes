8c91759db1d2c89099f67c36e00e669453ef200e
Aluno <aluno@etec.sp.gov.br>
2023-09-04 20:49:14 -0300

    Aula 04/09/2023

--- PWFilmes.Domain/Categoria.cs
+++ PWFilmes.Domain/Categoria.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace PWFilmes.Presentation
+namespace PWFilmes.Domain
 {
-    public class Categoria
+        public class Categoria
     {
-        public int Codigo { get; set;}
+        public int Codigo { get; set; }
         public string Descricao { get; set; }
         public string Cor { get; set; }
 
